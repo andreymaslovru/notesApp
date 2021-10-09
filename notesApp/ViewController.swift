45dff87a7af6c8d353959f077818e15399dcee3c
Andrey Maslov <andrey.ma2001@gmail.com>
2021-10-09 11:49:31 +0700

    launching app, added main screen, and check for isEmpty elements in models array

--- notesApp/ViewController.swift
+++ notesApp/ViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var models: [(title: String, note: String)] = []
 
+    
+    @IBOutlet weak var labelNoNotes: UILabel!
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        table.delegate = self
+        table.dataSource = self
+        title="Notes App"
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        models.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = models[indexPath.row].title
+        cell.detailTextLabel?.text = models[indexPath.row].note
+        return cell
     }
 
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "note") as? NoteViewController else { return }
+        vc.title = "Note"
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
